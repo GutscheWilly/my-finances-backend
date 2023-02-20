9f96a943fe89e5369a92b7f83b6ea125dc6c81bb
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-20 15:07:07 -0300

    Create project

---
+++ src/test/java/com/gutsche/myFinances/MyFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutsche.myFinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
