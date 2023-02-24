5e3d9d6fc018bcfab2282843bf9e9da2486b695f
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-24 13:39:29 -0300

    Test: Login user successful

--- src/test/java/com/gutsche/myFinances/service/UserServiceTest.java
+++ src/test/java/com/gutsche/myFinances/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.gutsche.myFinances.service;
 
+import com.gutsche.myFinances.model.entity.User;
 import com.gutsche.myFinances.model.repository.UserRepository;
 import com.gutsche.myFinances.service.exceptions.BusinessRuleException;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,8 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @Profile("test")
@@ -23,6 +26,19 @@ public class UserServiceTest {
     @BeforeEach
     public void setup() {
         userService = new UserServiceImplementation(userRepository);
+    }
+
+    @Test
+    public void shouldLoginUserSuccessful() {
+        String email = "user@gmail.com";
+        String password = "123456";
+
+        User user = User.builder().id(1L).name("user").email(email).password(password).build();
+        Mockito.when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+
+        User returnedUser = userService.validateLogin(email, password);
+
+        Assertions.assertEquals(returnedUser, user);
     }
 
     @Test
