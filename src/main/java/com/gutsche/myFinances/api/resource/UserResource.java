e1d7d64b462807dad3f7bab785ca08cd67420557
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-27 20:21:43 -0300

    POST: Register User

--- src/main/java/com/gutsche/myFinances/api/resource/UserResource.java
+++ src/main/java/com/gutsche/myFinances/api/resource/UserResource.java
@@ -1,5 +1,14 @@
 package com.gutsche.myFinances.api.resource;
 
+import com.gutsche.myFinances.api.dto.UserDTO;
+import com.gutsche.myFinances.model.entity.User;
+import com.gutsche.myFinances.service.UserService;
+import com.gutsche.myFinances.service.exceptions.BusinessRuleException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserResource {
 
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        User user = buildUser(userDTO);
+
+        try {
+            User registeredUser = userService.registerUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+        }
+        catch (BusinessRuleException businessRuleException) {
+            return ResponseEntity.badRequest().body(businessRuleException.getMessage());
+        }
+    }
+
+    private User buildUser(UserDTO userDTO) {
+        return User.builder()
+                .name(userDTO.getName())
+                .email(userDTO.getEmail())
+                .password(userDTO.getPassword())
+                .build();
+    }
 }
