b44e3e4b860822e8d87ac9be1cb84d00c35812a8
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-27 21:43:25 -0300

    POST: Validate Login

--- src/main/java/com/gutsche/myFinances/api/resource/UserResource.java
+++ src/main/java/com/gutsche/myFinances/api/resource/UserResource.java
@@ -4,13 +4,11 @@ import com.gutsche.myFinances.api.dto.UserDTO;
 import com.gutsche.myFinances.model.entity.User;
 import com.gutsche.myFinances.service.UserService;
 import com.gutsche.myFinances.service.exceptions.BusinessRuleException;
+import com.gutsche.myFinances.service.exceptions.LoginException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,6 +19,17 @@ public class UserResource {
     @Autowired
     public UserResource(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> validateLogin(@RequestBody UserDTO userDTO) {
+        try {
+            User loggedUser = userService.validateLogin(userDTO.getEmail(), userDTO.getPassword());
+            return ResponseEntity.ok().body(loggedUser);
+        }
+        catch (LoginException loginException) {
+            return ResponseEntity.badRequest().body(loginException.getMessage());
+        }
     }
 
     @PostMapping
