32a8c73e274edc997d11d177725c7d588735bcf7
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-06 18:23:49 -0300

    Test: Save launch successful

---
+++ src/test/java/com/gutsche/myFinances/model/repository/LaunchRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.gutsche.myFinances.model.repository;
+
+import com.gutsche.myFinances.model.entity.Launch;
+import com.gutsche.myFinances.model.entity.enums.LaunchStatus;
+import com.gutsche.myFinances.model.entity.enums.LaunchType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class LaunchRepositoryTest {
+
+    @Autowired
+    private LaunchRepository launchRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void shouldSaveLaunchSuccessful() {
+        Launch launch = buildLaunch();
+
+        Launch savedLaunch = launchRepository.save(launch);
+
+        Assertions.assertNotEquals(null, savedLaunch.getId());
+    }
+
+    private static Launch buildLaunch() {
+        return Launch.builder()
+                .month(1)
+                .year(2023)
+                .description("Test launch!")
+                .value(BigDecimal.valueOf(100))
+                .type(LaunchType.REVENUE)
+                .status(LaunchStatus.PENDENT)
+                .build();
+    }
+}
