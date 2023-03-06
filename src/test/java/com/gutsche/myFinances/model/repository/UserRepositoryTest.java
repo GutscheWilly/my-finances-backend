2887f86136cd71360c7475c55576076a39ae2c33
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-06 20:08:34 -0300

    Refactor User Repository Test

--- src/test/java/com/gutsche/myFinances/model/repository/UserRepositoryTest.java
+++ src/test/java/com/gutsche/myFinances/model/repository/UserRepositoryTest.java
@@ -1,75 +1,76 @@
 package com.gutsche.myFinances.model.repository;
 
 import com.gutsche.myFinances.model.entity.User;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
-@Profile("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TestEntityManager testEntityManager;
+
     @Test
     public void checkRegisteredEmail() {
-        resetContext();
-        User user = createUser();
-        userRepository.save(user);
+        User user = createAndPersistUser();
 
         String registeredEmail = user.getEmail();
         boolean isRegisteredEmail = userRepository.existsByEmail(registeredEmail);
 
-        Assertions.assertThat(isRegisteredEmail).isTrue();
+        Assertions.assertTrue(isRegisteredEmail);
     }
 
     @Test
     public void checkUnregisteredEmail() {
-        resetContext();
-
         String unregisteredEmail = "user@gmail.com";
         boolean isRegisteredEmail = userRepository.existsByEmail(unregisteredEmail);
 
-        Assertions.assertThat(isRegisteredEmail).isFalse();
+        Assertions.assertFalse(isRegisteredEmail);
     }
 
     @Test
     public void shouldCreateUserIdAfterRegister() {
-        resetContext();
         User user = createUser();
-
         User userAfterRegister = userRepository.save(user);
         Long id = userAfterRegister.getId();
 
-        Assertions.assertThat(id).isNotNull();
+        Assertions.assertNotNull(id);
     }
 
     @Test
     public void shouldFindRegisteredUserByEmail() {
-        resetContext();
-        User user = createUser();
-        userRepository.save(user);
+        User user = createAndPersistUser();
 
         String registeredEmail = user.getEmail();
         boolean isUserFoundByEmail = userRepository.findByEmail(registeredEmail).isPresent();
 
-        Assertions.assertThat(isUserFoundByEmail).isTrue();
+        Assertions.assertTrue(isUserFoundByEmail);
     }
 
     @Test
     public void shouldNotFindUserByEmail() {
-        resetContext();
-
         String unregisteredEmail = "user@gmail.com";
         boolean isUserFoundByEmail = userRepository.findByEmail(unregisteredEmail).isPresent();
 
-        Assertions.assertThat(isUserFoundByEmail).isFalse();
+        Assertions.assertFalse(isUserFoundByEmail);
+    }
+
+    private User createAndPersistUser() {
+        User user = createUser();
+        return testEntityManager.persist(user);
     }
 
     private static User createUser() {
@@ -78,9 +79,5 @@ public class UserRepositoryTest {
                 .email("user@gmail.com")
                 .password("123456")
                 .build();
-    }
-
-    private void resetContext() {
-        userRepository.deleteAll();
     }
 }
