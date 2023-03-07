a0e7fc39472c9fc46ba67e0baf74053c78dc7f78
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-06 22:20:39 -0300

    Test: Save launch

--- src/test/java/com/gutsche/myFinances/service/LaunchServiceTest.java
+++ src/test/java/com/gutsche/myFinances/service/LaunchServiceTest.java
@@ -1,12 +1,20 @@
 package com.gutsche.myFinances.service;
 
+import com.gutsche.myFinances.model.entity.Launch;
+import com.gutsche.myFinances.model.entity.User;
+import com.gutsche.myFinances.model.entity.enums.LaunchType;
 import com.gutsche.myFinances.model.repository.LaunchRepository;
 import com.gutsche.myFinances.service.implementation.LaunchServiceImplementation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -17,4 +25,31 @@ public class LaunchServiceTest {
 
     @MockBean
     private LaunchRepository launchRepository;
+
+    @Test
+    public void shouldSaveLaunchSuccessful() {
+        Launch launch = buildLaunch();
+
+        Mockito.doNothing().when(launchService).validateLaunch(launch);
+
+        Launch savedLaunch = buildLaunch();
+        savedLaunch.setId(1L);
+
+        Mockito.when(launchRepository.save(launch)).thenReturn(savedLaunch);
+
+        Launch returnedLaunch = launchService.save(launch);
+
+        Assertions.assertEquals(returnedLaunch, savedLaunch);
+    }
+
+    private static Launch buildLaunch() {
+        return Launch.builder()
+                .description("Test launch")
+                .month(2)
+                .year(2023)
+                .value(BigDecimal.valueOf(100))
+                .user(User.builder().id(1L).name("user").email("user@gamil.com").password("1234").build())
+                .type(LaunchType.REVENUE)
+                .build();
+    }
 }
