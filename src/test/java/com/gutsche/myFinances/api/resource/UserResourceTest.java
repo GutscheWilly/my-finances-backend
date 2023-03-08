c32a437b5f7b46b9f90f1ffedfac4eac0aeac13a
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-08 13:54:49 -0300

    Create User Resource Test

---
+++ src/test/java/com/gutsche/myFinances/api/resource/UserResourceTest.java
@@ -0,0 +1,21 @@
+package com.gutsche.myFinances.api.resource;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers = UserResource.class)
+@AutoConfigureMockMvc
+public class UserResourceTest {
+
+    private static final String API = "/api/users";
+
+    @Autowired
+    private MockMvc mockMvc;
+}
