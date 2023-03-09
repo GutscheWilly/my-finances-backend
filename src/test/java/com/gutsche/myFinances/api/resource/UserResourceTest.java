0937daebe158f92e32376cbfb3fcd7ce49eb0b39
Willy Gutsche <willy.gutsche@unifesp.br>
2023-03-08 23:54:13 -0300

    Test: Return bad request when is not possible register user

--- src/test/java/com/gutsche/myFinances/api/resource/UserResourceTest.java
+++ src/test/java/com/gutsche/myFinances/api/resource/UserResourceTest.java
@@ -5,6 +5,7 @@ import com.gutsche.myFinances.api.dto.UserDTO;
 import com.gutsche.myFinances.model.entity.User;
 import com.gutsche.myFinances.service.LaunchService;
 import com.gutsche.myFinances.service.UserService;
+import com.gutsche.myFinances.service.exceptions.BusinessRuleException;
 import com.gutsche.myFinances.service.exceptions.LoginException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -114,5 +115,26 @@ public class UserResourceTest {
                 MockMvcResultMatchers.jsonPath("name").value(userAfterRegister.getName()),
                 MockMvcResultMatchers.jsonPath("email").value(userAfterRegister.getEmail())
         );
+    }
+
+    @Test
+    public void shouldReturnBadRequestWhenIsNotPossibleRegisterUser() throws Exception {
+        String name = "user";
+        String email = "user@gmail.com";
+        String password = "123456";
+
+        UserDTO userDTO = UserDTO.builder().name(name).email(email).password(password).build();
+
+        Mockito.when(userService.registerUser(Mockito.any(User.class))).thenThrow(BusinessRuleException.class);
+
+        String userJson = new ObjectMapper().writeValueAsString(userDTO);
+
+        MockHttpServletRequestBuilder userRequest = MockMvcRequestBuilders
+                .post(PATH_API)
+                .accept(JSON)
+                .contentType(JSON)
+                .content(userJson);
+
+        mockMvc.perform(userRequest).andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 }
